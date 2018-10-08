4c28d71c1c9c9c2e2965068df57d7faa22c14dae
theo <theo.giraudet@orange.fr>
2018-10-08 19:28:59 +0200

    Package + Resources

---
+++ src/main/java/net/gunivers/cmdloader/Main.java
@@ -0,0 +1,11 @@
+package net.gunivers.cmdloader;
+
+public class Main {
+
+	public static void main(String[] args) {
+		System.out.println("a");
+		System.out.println(Main.class.getResource("/Test.json"));
+
+	}
+
+}
