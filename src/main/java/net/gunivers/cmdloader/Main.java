d1c282910f96cb4c1877de82208a9ec326b013f2
theo <theo.giraudet@orange.fr>
2018-10-22 23:11:03 +0200

    Fixe

--- src/main/java/net/gunivers/cmdloader/Main.java
+++ src/main/java/net/gunivers/cmdloader/Main.java
@@ -30,34 +30,30 @@ public class Main {
 	// À revoir
 	@SuppressWarnings("unchecked")
 	public static void readJSONObjectAndDisp(HashMap<String, Object> hm, int numberTab) throws ParseException {
-		for (Entry<String, Object> entry : hm.entrySet()) {
-			System.out.println(Strings.repeat("\t", numberTab) + entry.getKey());
-
-			if (entry.getValue() instanceof JSONObject) {
+		for (Entry<String, Object> entry : hm.entrySet()) { //Tant qu'il y a des éléments sur un même niveau
+			System.out.println(Strings.repeat("\t", numberTab) + entry.getKey()); //On print dans tout les cas la clé
+			if (entry.getValue() instanceof JSONObject) { //Si c'est un JSON Object, on convertir en HashMap, puis on applique la récursive dessus
 				HashMap<String, Object> hm2 = new HashMap<>();
 				hm2.putAll((JSONObject) entry.getValue());
-
 				readJSONObjectAndDisp(hm2, numberTab + 1);
-
-			} else if (entry.getValue() instanceof JSONArray) {
-				readJSONArrayAndDisp((JSONArray) entry.getValue(), numberTab + 1);
-			} else {
-				System.out.println(Strings.repeat("\t", numberTab + 1) + entry.getValue());
-			}
+			} else if (entry.getValue() instanceof JSONArray)
+				readJSONArrayAndDisp((JSONArray) entry.getValue(), numberTab + 1); //Sinon, si c'est un JSON Array, on appelle la méthode faite pour les lire
+			else
+				System.out.println(Strings.repeat("\t", numberTab + 1) + entry.getValue()); //Sinon, on print la valeur
 
 		}
 	}
 
 	@SuppressWarnings("unchecked")
 	public static void readJSONArrayAndDisp(JSONArray array, int numberTab) {
-		for (Object o : array) {
-			if (o instanceof JSONObject) {
+		for (Object o : array) { //On boucle sur le tableau
+			if (o instanceof JSONObject) { //Si c'est un JSON Object, on appelle la méthode les traitants
 				try {
 					readJSONObjectAndDisp((HashMap<String, Object>) o, numberTab);
 				} catch (ParseException e) {
 					e.printStackTrace();
 				}
-			} else
+			} else //Sinon, on print la valeur
 				System.out.println(Strings.repeat("\t", numberTab + 1) + o);
 		}
 	}
